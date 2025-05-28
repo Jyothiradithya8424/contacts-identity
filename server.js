5c3e6147dc555633ec414628c666c7d85e34ee8a
Jyothiradithya8424 <165758758+Jyothiradithya8424@users.noreply.github.com>
2025-05-28 20:18:45 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const contactRoutes = require("./routes/identify");
+const { ensureDatabase, initializeSchema } = require("./db");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const startApp = async () => {
+  await ensureDatabase();
+  await initializeSchema();
+
+  app.use("/", contactRoutes);
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running at http://localhost:${PORT}`);
+  });
+};
+
+startApp();
